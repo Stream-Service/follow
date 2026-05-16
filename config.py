28e00f54698542a9a66b4ecdba244672b672d814
Hritik <hritikchoukikar2000@gmail.com>
2026-05-16 12:32:44 +0530

    upadeted code

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     NEO4J_AUTH:str
     NEO4J_PASSWORD:str
     NEO4J_URI:str
-    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
+    KAFKA_BOOTSTRAP_SERVERS: str 
     
 
 
